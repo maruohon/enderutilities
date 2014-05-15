932ca52d0aaf44033da7405e2e8fd403073fd4ef
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2014-05-15 10:01:17 +0300

    Create the mod skeleton

---
+++ src/main/java/fi/dy/masa/minecraft/mods/enderutilities/EnderUtilities.java
@@ -0,0 +1,34 @@
+package fi.dy.masa.minecraft.mods.enderutilities;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import fi.dy.masa.minecraft.mods.enderutilities.reference.Reference;
+
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION)
+public class EnderUtilities
+{
+	@Instance(Reference.MOD_ID)
+	public static EnderUtilities instance;
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+	}
+
+/*
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+	}
+*/
+}
---
+++ src/main/java/fi/dy/masa/minecraft/mods/enderutilities/reference/Reference.java
@@ -0,0 +1,8 @@
+package fi.dy.masa.minecraft.mods.enderutilities.reference;
+
+public class Reference
+{
+	public static final String MOD_ID = "enderutilities";
+	public static final String MOD_NAME = "Ender Utilities";
+	public static final String MOD_VERSION = "0.1";
+}
