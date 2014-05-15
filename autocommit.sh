e4c07601dc0ee696a4763da74989c62c846c5e08
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2014-05-15 09:28:27 +0300

    Update the autocommit script

--- autocommit.sh
+++ autocommit.sh
@@ -1,26 +1,49 @@
 #!/bin/bash
 
-AUTOCOMMITREPO="C:\Users\masa\Desktop\minecraft_modding\modjam\modjam4_autocommit.git"
+AUTOCOMMITREPO="C:\Users\masa\Desktop\minecraft_modding\modjam\modjam4_autocommit.git\.git"
 WORKDIR="C:\Users\masa\Desktop\minecraft_modding\modjam\modjam4.git"
 TIMESTAMPFILE="${WORKDIR}/.autocommit.txt"
+
+DONE="false"
 
 while true
 do
 	M=`date "+%M"`
 	S=`date "+%S"`
 
-	if [ "x${S}" = "x00" ]
+	# if [ "x${S}" = "x00" ]
+	# then
+	# 	# Commit quarter hourly
+	# 	if [ "x${M}" = "x00" ] || [ "x${M}" = "x15" ] || [ "x${M}" = "x30" ] || [ "x${M}" = "x45" ]
+	# 	then
+	# 		TIMESTAMP=`date "+%Y-%m-%d_%H:%M:%S"`
+	# 		echo "Auto-commit @ $TIMESTAMP" > ${TIMESTAMPFILE}
+	# 		# echo "Timestamp: $TIMESTAMP"
+	# 		git --work-tree "${WORKDIR}" --git-dir "${AUTOCOMMITREPO}" add -A .
+	# 		git --work-tree "${WORKDIR}" --git-dir "${AUTOCOMMITREPO}" add -f "${TIMESTAMPFILE}"
+	# 		git --work-tree "${WORKDIR}" --git-dir "${AUTOCOMMITREPO}" commit -m "Auto-commit @ ${TIMESTAMP}"
+	# 	fi
+	# fi
+	# sleep 1
+
+	# Commit quarter hourly
+	if [ "x${M}" = "x00" ] || [ "x${M}" = "x15" ] || [ "x${M}" = "x30" ] || [ "x${M}" = "x45" ]
 	then
-		# Commit quarter hourly
-		if [ "x${M}" = "x00" ] || [ "x${M}" = "x15" ] || [ "x${M}" = "x30" ] || [ "x${M}" = "x45" ]
+		if [ "x${DONE}" == "xfalse" ]
 		then
 			TIMESTAMP=`date "+%Y-%m-%d_%H:%M:%S"`
-			#echo "Auto-commit @ $TIMESTAMP" > ${TIMESTAMPFILE}
-			echo "Timestamp: $TIMESTAMP"
-            git --work-tree "${WORKDIR}" --git-dir "${AUTOCOMMITREPO}" add -A .
-            git --work-tree "${WORKDIR}" --git-dir "${AUTOCOMMITREPO}" add -f "${TIMESTAMPFILE}"
-            git --work-tree "${WORKDIR}" --git-dir "${AUTOCOMMITREPO}" commit -m "Auto-commit @ ${TIMESTAMP}"
+			echo "Auto-commit @ $TIMESTAMP" > ${TIMESTAMPFILE}
+			# echo "Timestamp: $TIMESTAMP"
+
+			git --work-tree "${WORKDIR}" --git-dir "${AUTOCOMMITREPO}" add -A .
+			git --work-tree "${WORKDIR}" --git-dir "${AUTOCOMMITREPO}" add -f "${TIMESTAMPFILE}"
+			git --work-tree "${WORKDIR}" --git-dir "${AUTOCOMMITREPO}" commit -m "Auto-commit @ ${TIMESTAMP}"
+
+			DONE="true"
 		fi
+	else
+		DONE="false"
 	fi
-	sleep 1
+
+	sleep 30
 done
