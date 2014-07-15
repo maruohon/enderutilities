ad0254c99204b1d3c72db9e011f6016ed7c499aa
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2014-07-15 08:33:00 +0300

    Ender Arrows: Fix and adjust the spawn location, increase the speed.
    The recent change to the spawn location caused the arrows to also hit
    about a block to the right from the center. The arrows now spawn
    closer to the line of sight, but further ahead of the player.
    
    Also increased the arrow speed some more, because OP ftw :p

--- src/main/java/fi/dy/masa/enderutilities/entity/EntityEnderArrow.java
+++ src/main/java/fi/dy/masa/enderutilities/entity/EntityEnderArrow.java
@@ -117,15 +117,17 @@ public class EntityEnderArrow extends Entity implements IProjectile
 
 		this.setSize(0.5F, 0.5F);
 		this.setLocationAndAngles(par2EntityLivingBase.posX, par2EntityLivingBase.posY + (double)par2EntityLivingBase.getEyeHeight(), par2EntityLivingBase.posZ, par2EntityLivingBase.rotationYaw, par2EntityLivingBase.rotationPitch);
-		this.posX -= (double)(MathHelper.cos(this.rotationYaw / 180.0F * (float)Math.PI) * 0.7F);
+		this.posX -= (double)(MathHelper.sin(this.rotationYaw / 180.0F * (float)Math.PI) * 1.0f);
+		this.posX -= (double)(MathHelper.cos(this.rotationYaw / 180.0F * (float)Math.PI) * 0.1f);
 		this.posY -= 0.10000000149011612D;
-		this.posZ -= (double)(MathHelper.sin(this.rotationYaw / 180.0F * (float)Math.PI) * 0.7F);
+		this.posZ += (double)(MathHelper.cos(this.rotationYaw / 180.0F * (float)Math.PI) * 1.0f);
+		this.posZ -= (double)(MathHelper.sin(this.rotationYaw / 180.0F * (float)Math.PI) * 0.1f);
 		this.setPosition(this.posX, this.posY, this.posZ);
 		this.yOffset = 0.0F;
 		this.motionX = (double)(-MathHelper.sin(this.rotationYaw / 180.0F * (float)Math.PI) * MathHelper.cos(this.rotationPitch / 180.0F * (float)Math.PI));
 		this.motionZ = (double)(MathHelper.cos(this.rotationYaw / 180.0F * (float)Math.PI) * MathHelper.cos(this.rotationPitch / 180.0F * (float)Math.PI));
 		this.motionY = (double)(-MathHelper.sin(this.rotationPitch / 180.0F * (float)Math.PI));
-		this.setThrowableHeading(this.motionX, this.motionY, this.motionZ, par3 * 1.7F, 1.0F);
+		this.setThrowableHeading(this.motionX, this.motionY, this.motionZ, par3 * 1.8F, 1.0F);
 	}
 
 	protected void entityInit()
