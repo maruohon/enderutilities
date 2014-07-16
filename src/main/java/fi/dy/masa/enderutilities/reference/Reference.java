6b4fa949975f9f432e45015e5f5a242c7a75746d
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2014-07-17 01:36:00 +0300

    Update build.gradle to append date to dev builds

--- src/main/java/fi/dy/masa/enderutilities/reference/Reference.java
+++ src/main/java/fi/dy/masa/enderutilities/reference/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
 	public static final String MOD_ID = "enderutilities";
 	public static final String MOD_NAME = "Ender Utilities";
-	public static final String MOD_VERSION = "@MODVERSION@";
+	public static final String MOD_VERSION = "@MOD_VERSION@";
 	public static final String PROXY_CLASS_CLIENT = "fi.dy.masa.enderutilities.proxy.ClientProxy";
 	public static final String PROXY_CLASS_SERVER = "fi.dy.masa.enderutilities.proxy.ServerProxy";
 }
