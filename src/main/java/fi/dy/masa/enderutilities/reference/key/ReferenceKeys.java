cb36b31b2cfecc9869cc603145d73dc18d556325
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2014-07-17 04:57:30 +0300

    Change the default Toggle Mode key from middle mouse to F.

--- src/main/java/fi/dy/masa/enderutilities/reference/key/ReferenceKeys.java
+++ src/main/java/fi/dy/masa/enderutilities/reference/key/ReferenceKeys.java
@@ -1,5 +1,7 @@
 package fi.dy.masa.enderutilities.reference.key;
 
+import org.lwjgl.input.Keyboard;
+
 import fi.dy.masa.enderutilities.reference.Reference;
 
 public class ReferenceKeys
@@ -8,7 +10,7 @@ public class ReferenceKeys
 
 	public static final String KEYBIND_TOGGLE_MODE = "key." + Reference.MOD_ID + ".togglemode";
 
-	public static final int KEYBIND_DEFAULT_TOGGLE_MODE = -98; // Middle mouse
+	public static final int KEYBIND_DEFAULT_TOGGLE_MODE = Keyboard.KEY_F;
 
 	// These are used to identify the pressed key on the server side. They have nothing to do with actual key codes.
 	public static final int KEYBIND_ID_TOGGLE_MODE = 0;
