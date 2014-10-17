8dbf1501ea9e87ea14382af750db98dd52090314
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2014-10-17 22:55:12 +0300

    Add an interface and some skeleton methods for modular items.

---
+++ src/main/java/fi/dy/masa/enderutilities/item/base/IModular.java
@@ -0,0 +1,38 @@
+package fi.dy.masa.enderutilities.item.base;
+
+import java.util.List;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+
+public interface IModular
+{
+	public static final int MODULE_ENDERCORE = 0x01;
+	public static final int MODULE_ENDECAPACITOR = 0x02;
+	public static final int MODULE_LINKCRYSTAL = 0x04;
+	public static final int MODULE_PERSISTANCE = 0x08;
+
+	/* Return whether the given module type has been installed. */
+	public boolean hasModule(ItemStack stack, int moduleType);
+
+	/* Returns the number of installed modules of the given type. */
+	public int getModuleCount(ItemStack stack, int moduleType);
+
+	/* Returns a bitmask of the installed module types. Used for quicker checking of what is installed. */
+	public int getInstalledModulesMask(ItemStack stack);
+
+	/* Returns the (max, if multiple) tier of the installed module. */
+	public int getModuleTier(ItemStack stack, int moduleType);
+
+	/* Returns the ItemStack of the (selected, if multiple) given module type. */
+	public ItemStack getSelectedModuleStack(ItemStack stack, int moduleType);
+
+	/* Returns a list of all the installed modules. */
+	public List<NBTTagCompound> getAllModules(ItemStack stack);
+
+	/* Sets the modules to the ones provided in the list. */
+	public ItemStack setAllModules(ItemStack stack, List<NBTTagCompound> modules);
+
+	/* Sets the module indicated by the position to the one provided in the compound tag. */
+	public ItemStack setModule(ItemStack stack, int index, NBTTagCompound nbt);
+}
---
+++ src/main/java/fi/dy/masa/enderutilities/util/nbt/ItemModular.java
@@ -0,0 +1,65 @@
+package fi.dy.masa.enderutilities.util.nbt;
+
+import java.util.List;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+
+public class ItemModular
+{
+	/* Return whether the given module type has been installed. */
+	public static boolean hasModule(ItemStack stack, int moduleType)
+	{
+		if (stack == null) { return false; }
+		return false;
+	}
+
+	/* Returns the number of installed modules of the given type. */
+	public static int getModuleCount(ItemStack stack, int moduleType)
+	{
+		if (stack == null) { return 0; }
+		return 0;
+	}
+
+	/* Returns a bitmask of the installed module types. Used for quicker checking of what is installed. */
+	public static int getInstalledModulesMask(ItemStack stack)
+	{
+		if (stack == null) { return 0; }
+		return 0;
+	}
+
+	/* Returns the (max, if multiple) tier of the installed module. */
+	public static int getModuleTier(ItemStack stack, int moduleType)
+	{
+		if (stack == null) { return 0; }
+		return 0;
+	}
+
+	/* Returns the ItemStack of the (selected, if multiple) given module type. */
+	public static ItemStack getSelectedModuleStack(ItemStack stack, int moduleType)
+	{
+		if (stack == null) { return null; }
+		return null;
+	}
+
+	/* Returns a list of all the installed modules. */
+	public static List<NBTTagCompound> getAllModules(ItemStack stack)
+	{
+		if (stack == null) { return null; }
+		return null;
+	}
+
+	/* Sets the modules to the ones provided in the list. */
+	public static ItemStack setAllModules(ItemStack stack, List<NBTTagCompound> modules)
+	{
+		if (stack == null) { return null; }
+		return stack;
+	}
+
+	/* Sets the module indicated by the position to the one provided in the compound tag. */
+	public static ItemStack setModule(ItemStack stack, int index, NBTTagCompound nbt)
+	{
+		if (stack == null) { return null; }
+		return stack;
+	}
+}
