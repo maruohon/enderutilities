ad6d64985213190c1a041ee28087dc45960e8f0b
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2015-05-14 00:39:05 +0300

    Add recipies for Energy Bridge blocks.

--- src/main/java/fi/dy/masa/enderutilities/init/EnderUtilitiesBlocks.java
+++ src/main/java/fi/dy/masa/enderutilities/init/EnderUtilitiesBlocks.java
@@ -25,6 +25,7 @@ public class EnderUtilitiesBlocks
         ItemStack chest = new ItemStack(Blocks.chest);
         ItemStack craftingtable = new ItemStack(Blocks.crafting_table);
         ItemStack furnace = new ItemStack(Blocks.furnace);
+        ItemStack glass = new ItemStack(Blocks.glass);
         ItemStack hopper = new ItemStack(Blocks.hopper);
         ItemStack obsidian = new ItemStack(Blocks.obsidian);
         //ItemStack pearl = new ItemStack(Items.ender_pearl);
@@ -33,13 +34,13 @@ public class EnderUtilitiesBlocks
 
         ItemStack alloy0 = new ItemStack(EnderUtilitiesItems.enderPart, 1, 0);
         ItemStack alloy1 = new ItemStack(EnderUtilitiesItems.enderPart, 1, 1);
-        //ItemStack alloy2 = new ItemStack(EnderUtilitiesItems.enderPart, 1, 2);
+        ItemStack alloy2 = new ItemStack(EnderUtilitiesItems.enderPart, 1, 2);
         ItemStack core0 = new ItemStack(EnderUtilitiesItems.enderPart, 1, 10);
         //ItemStack core1 = new ItemStack(EnderUtilitiesItems.enderPart, 1, 11);
         //ItemStack core2 = new ItemStack(EnderUtilitiesItems.enderPart, 1, 12);
         //ItemStack active_core0 = new ItemStack(EnderUtilitiesItems.enderPart, 1, 15);
-        //ItemStack active_core1 = new ItemStack(EnderUtilitiesItems.enderPart, 1, 16);
-        //ItemStack active_core2 = new ItemStack(EnderUtilitiesItems.enderPart, 1, 17);
+        ItemStack active_core1 = new ItemStack(EnderUtilitiesItems.enderPart, 1, 16);
+        ItemStack active_core2 = new ItemStack(EnderUtilitiesItems.enderPart, 1, 17);
         //ItemStack ender_stick = new ItemStack(EnderUtilitiesItems.enderPart, 1, 20);
         //ItemStack rope = new ItemStack(EnderUtilitiesItems.enderPart, 1, 21);
 
@@ -59,12 +60,15 @@ public class EnderUtilitiesBlocks
 
         if (Configs.disableRecipeEnergyBridgeTransmitter.getBoolean(false) == false && Configs.disableBlockMachine_1.getBoolean(false) == false)
         {
+            GameRegistry.addRecipe(new ItemStack(machine_1, 1, 0), "AGA", "ACA", "AGA", 'A', alloy2, 'G', glass, 'C', active_core2);
         }
         if (Configs.disableRecipeEnergyBridgeReceiver.getBoolean(false) == false && Configs.disableBlockMachine_1.getBoolean(false) == false)
         {
+            GameRegistry.addRecipe(new ItemStack(machine_1, 1, 1), "AGA", "GCG", "AGA", 'A', alloy2, 'G', glass, 'C', active_core2);
         }
         if (Configs.disableRecipeEnergyBridgeResonator.getBoolean(false) == false && Configs.disableBlockMachine_1.getBoolean(false) == false)
         {
+            GameRegistry.addRecipe(new ItemStack(machine_1, 1, 2), "AGA", "GCG", "AAA", 'A', alloy1, 'G', glass, 'C', active_core1);
         }
     }
 }
