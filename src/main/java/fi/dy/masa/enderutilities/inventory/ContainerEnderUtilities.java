bc68b7802e1ca52120c46d805b6dc690f589b619
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2016-01-09 11:27:19 +0200

    Minor clean-up to transferStackInSlot() and mergeItemStack()

--- src/main/java/fi/dy/masa/enderutilities/inventory/ContainerEnderUtilities.java
+++ src/main/java/fi/dy/masa/enderutilities/inventory/ContainerEnderUtilities.java
@@ -1,5 +1,6 @@
 package fi.dy.masa.enderutilities.inventory;
 
+import fi.dy.masa.enderutilities.util.InventoryUtils;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
@@ -19,7 +20,9 @@ public class ContainerEnderUtilities extends Container
     }
 
     /**
-     * Adds the "custom" inventory slots to the container (ie. the inventory that this container is for).
+     * Adds the "custom inventory" slots to the container (ie. the inventory that this container is for).
+     * This must be called before addPlayerInventorySlots() (ie. the order of slots in the container
+     * is important for the transferStackInSlot() method)!
      */
     protected void addCustomInventorySlots()
     {
@@ -70,32 +73,32 @@ public class ContainerEnderUtilities extends Container
 
     public ItemStack transferStackInSlot(EntityPlayer player, int slotNum, int invSize)
     {
-        ItemStack stack = null;
-        Slot slot = this.getSlot(slotNum);
+        ItemStack stackOrig = null;
+        Slot slot = (slotNum >= 0 && slotNum < this.inventorySlots.size()) ? this.getSlot(slotNum) : null;
 
         // Slot clicked on has items
-        if (slot != null && slot.getHasStack() == true)
+        if (slot != null && slot.getHasStack() == true && slot.canTakeStack(player) == true)
         {
-            ItemStack stackInSlot = slot.getStack();
-            stack = stackInSlot.copy();
+            stackOrig = slot.getStack();
+            ItemStack stackTmp = stackOrig.copy();
 
             // Clicked on a slot is in the "external" inventory
             if (slotNum < invSize)
             {
                 // Try to merge the stack into the player inventory
-                if (this.mergeItemStack(stackInSlot, invSize, this.getNumMergableSlots(invSize), true) == false)
+                if (this.mergeItemStack(stackTmp, invSize, this.getNumMergableSlots(invSize), true) == false)
                 {
                     return null;
                 }
             }
             // Clicked on slot is in the player inventory, try to merge the stack to the external inventory
-            else if (this.mergeItemStack(stackInSlot, 0, invSize, false) == false)
+            else if (this.mergeItemStack(stackTmp, 0, invSize, false) == false)
             {
                 return null;
             }
 
             // All items moved, empty the slot
-            if (stackInSlot.stackSize == 0)
+            if (stackTmp.stackSize == 0)
             {
                 slot.putStack(null);
             }
@@ -105,16 +108,16 @@ public class ContainerEnderUtilities extends Container
                 slot.onSlotChanged();
             }
 
-            // No items were moved
-            if (stackInSlot.stackSize == stack.stackSize)
+            // No items were moved FIXME this is redundant, right? In this case we should have already returned above
+            if (stackTmp.stackSize == stackOrig.stackSize)
             {
                 return null;
             }
 
-            slot.onPickupFromSlot(player, stackInSlot);
+            slot.onPickupFromSlot(player, stackTmp);
         }
 
-        return stack;
+        return stackOrig;
     }
 
     /**
@@ -128,10 +131,9 @@ public class ContainerEnderUtilities extends Container
     @Override
     protected boolean mergeItemStack(ItemStack stack, int slotStart, int slotEndExclusive, boolean reverse)
     {
-        boolean successful = false;
+        boolean movedItems = false;
         int slotIndex = (reverse == true ? slotEndExclusive - 1 : slotStart);
-        int maxSizeStackInv = 1;
-        int maxSizeTmp = maxSizeStackInv;
+        int maxSize = 1;
 
         Slot slot;
         ItemStack existingStack;
@@ -140,28 +142,24 @@ public class ContainerEnderUtilities extends Container
         while (stack.stackSize > 0 && slotIndex >= slotStart && slotIndex < slotEndExclusive)
         {
             slot = this.getSlot(slotIndex);
-            maxSizeStackInv = this.getMaxStackSizeFromSlotAndStack(slot, stack);
-            maxSizeTmp = Math.min(slot.getSlotStackLimit(), maxSizeStackInv);
             existingStack = slot.getStack();
+            maxSize = this.getMaxStackSizeFromSlotAndStack(slot, stack);
 
-            if (slot.isItemValid(stack) == true && existingStack != null
-                && existingStack.isItemEqual(stack) && ItemStack.areItemStackTagsEqual(stack, existingStack))
+            if (existingStack != null && slot.isItemValid(stack) == true && InventoryUtils.areItemStacksEqual(stack, existingStack) == true)
             {
-                int combinedSize = existingStack.stackSize + stack.stackSize;
-
-                if (combinedSize <= maxSizeTmp)
+                if ((existingStack.stackSize + stack.stackSize) <= maxSize)
                 {
+                    existingStack.stackSize += stack.stackSize;
                     stack.stackSize = 0;
-                    existingStack.stackSize = combinedSize;
-                    slot.putStack(existingStack); // Needed to call the setInventorySlotContents() method, which does special things on some machines
+                    slot.putStack(existingStack); // Needed to call setInventorySlotContents() and markDirty()
                     return true;
                 }
-                else if (existingStack.stackSize < maxSizeTmp)
+                else if (existingStack.stackSize < maxSize)
                 {
-                    stack.stackSize -= maxSizeTmp - existingStack.stackSize;
-                    existingStack.stackSize = maxSizeTmp;
-                    slot.putStack(existingStack); // Needed to call the setInventorySlotContents() method, which does special things on some machines
-                    successful = true;
+                    stack.stackSize -= maxSize - existingStack.stackSize;
+                    existingStack.stackSize = maxSize;
+                    slot.putStack(existingStack); // Needed to call setInventorySlotContents() and markDirty()
+                    movedItems = true;
                 }
             }
 
@@ -176,13 +174,12 @@ public class ContainerEnderUtilities extends Container
             while (slotIndex >= slotStart && slotIndex < slotEndExclusive)
             {
                 slot = this.getSlot(slotIndex);
-                maxSizeStackInv = this.getMaxStackSizeFromSlotAndStack(slot, stack);
-                maxSizeTmp = Math.min(slot.getSlotStackLimit(), maxSizeStackInv);
+                maxSize = this.getMaxStackSizeFromSlotAndStack(slot, stack);
                 existingStack = slot.getStack();
 
                 if (existingStack == null && slot.isItemValid(stack) == true)
                 {
-                    if (stack.stackSize <= maxSizeTmp)
+                    if (stack.stackSize <= maxSize)
                     {
                         slot.putStack(stack.copy());
                         stack.stackSize = 0;
@@ -191,10 +188,10 @@ public class ContainerEnderUtilities extends Container
                     else
                     {
                         ItemStack newStack = stack.copy();
-                        newStack.stackSize = maxSizeTmp;
-                        stack.stackSize -= maxSizeTmp;
+                        newStack.stackSize = maxSize;
+                        stack.stackSize -= maxSize;
                         slot.putStack(newStack);
-                        successful = true;
+                        movedItems = true;
                     }
                 }
 
@@ -202,6 +199,6 @@ public class ContainerEnderUtilities extends Container
             }
         }
 
-        return successful;
+        return movedItems;
     }
 }
