9096c256cc5856668989169e24f5253e62cea2db
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2016-01-29 08:01:43 +0200

    Ender Pearl: Return to player when killed in void, fixes #10.

--- src/main/java/fi/dy/masa/enderutilities/entity/EntityEnderPearlReusable.java
+++ src/main/java/fi/dy/masa/enderutilities/entity/EntityEnderPearlReusable.java
@@ -11,6 +11,7 @@ import net.minecraft.util.MathHelper;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.util.MovingObjectPosition.MovingObjectType;
 import net.minecraft.world.World;
+
 import fi.dy.masa.enderutilities.entity.base.EntityThrowableEU;
 import fi.dy.masa.enderutilities.setup.EnderUtilitiesItems;
 import fi.dy.masa.enderutilities.util.EntityUtils;
@@ -76,15 +77,25 @@ public class EntityEnderPearlReusable extends EntityThrowableEU implements IItem
     }
 
     @Override
+    protected void kill()
+    {
+        // Failed to add the pearl straight back to the thrower's inventory: drop the item in the world
+        if (this.returnToPlayersInventory() == false)
+        {
+            this.dropAsItem();
+        }
+
+        super.kill();
+    }
+
+    @Override
     public void onUpdate()
     {
         // The pearl has been dismounted, try to return the item to the thrower's inventory, or drop it as an item
         if (this.worldObj.isRemote == false && this.isElite == true && this.riddenByEntity == null)
         {
-            Entity thrower = this.getThrower();
-
-            // Failed to add the pearl straight back to the thrower's inventory: spawn it in the world
-            if (this.canPickUp == true && ((thrower instanceof EntityPlayer) == false || ((EntityPlayer)thrower).inventory.addItemStackToInventory(new ItemStack(EnderUtilitiesItems.enderPearlReusable, 1, 1)) == false))
+            // Failed to add the pearl straight back to the thrower's inventory: drop the item in the world
+            if (this.returnToPlayersInventory() == false)
             {
                 this.dropAsItem();
             }
@@ -99,66 +110,87 @@ public class EntityEnderPearlReusable extends EntityThrowableEU implements IItem
     @Override
     protected void onImpact(MovingObjectPosition mop)
     {
-        if (this.worldObj.isRemote == false)
+        if (this.worldObj.isRemote == true)
         {
-            Entity thrower = this.getThrower();
+            return;
+        }
 
-            // Thrower not found, drop the item if applicable and bail out
-            if (thrower == null)
-            {
-                if (this.canPickUp == true)
-                {
-                    this.dropAsItem();
-                }
+        Entity thrower = this.getThrower();
 
-                this.setDead();
-                return;
-            }
-
-            // Don't collide with the thrower or the entities in the 'stack' with the thrower
-            if (mop.typeOfHit == MovingObjectType.ENTITY && EntityUtils.doesEntityStackContainEntity(mop.entityHit, thrower) == true)
-            {
-                return;
-            }
-
-            if (mop.entityHit != null && mop.entityHit instanceof EntityLivingBase)
-            {
-                mop.entityHit.attackEntityFrom(DamageSource.causeThrownDamage(this, thrower), 0.0f);
-            }
-
-            // A regular pearl lands, teleport the thrower
-            if (this.isElite == false)
-            {
-                // If the thrower is currently riding an elite pearl, unmount the pearl
-                Entity bottom = EntityUtils.getBottomEntity(thrower);
-                if (bottom instanceof EntityEnderPearlReusable && bottom.riddenByEntity != null)
-                {
-                    bottom.riddenByEntity.mountEntity(null);
-                }
-
-                TeleportEntity.entityTeleportWithProjectile(thrower, this, mop, this.teleportDamage, true, true);
-            }
-            // An Elite pearl lands, which is still being ridden by something (see above)
-            else //if (this.riddenByEntity != null)
-            {
-                Entity entity = this.riddenByEntity;
-                entity.mountEntity(null);
-                TeleportEntity.entityTeleportWithProjectile(entity, this, mop, this.teleportDamage, true, true);
-            }
-
-            // Try to add the pearl straight back to the player's inventory
+        // Thrower not found, drop the item if applicable and bail out
+        if (thrower == null)
+        {
             if (this.canPickUp == true)
             {
-                int damage = (this.isElite == true ? 1 : 0);
-                // Failed to add the pearl straight back to the thrower's inventory, drop it as an item
-                if ((thrower instanceof EntityPlayer) == false || ((EntityPlayer)thrower).inventory.addItemStackToInventory(new ItemStack(EnderUtilitiesItems.enderPearlReusable, 1, damage)) == false)
-                {
-                    this.dropAsItem();
-                }
+                this.dropAsItem();
             }
 
             this.setDead();
+            return;
         }
+
+        // Don't collide with the thrower or the entities in the 'stack' with the thrower
+        if (mop.typeOfHit == MovingObjectType.ENTITY && EntityUtils.doesEntityStackContainEntity(mop.entityHit, thrower) == true)
+        {
+            return;
+        }
+
+        if (mop.entityHit != null && mop.entityHit instanceof EntityLivingBase)
+        {
+            mop.entityHit.attackEntityFrom(DamageSource.causeThrownDamage(this, thrower), 0.0f);
+        }
+
+        // A regular pearl lands, teleport the thrower
+        if (this.isElite == false)
+        {
+            // If the thrower is currently riding an elite pearl, unmount the pearl
+            Entity bottom = EntityUtils.getBottomEntity(thrower);
+            if (bottom instanceof EntityEnderPearlReusable && bottom.riddenByEntity != null)
+            {
+                bottom.riddenByEntity.mountEntity(null);
+            }
+
+            TeleportEntity.entityTeleportWithProjectile(thrower, this, mop, this.teleportDamage, true, true);
+        }
+        // An Elite pearl lands, which is still being ridden by something (see above)
+        else //if (this.riddenByEntity != null)
+        {
+            Entity entity = this.riddenByEntity;
+            entity.mountEntity(null);
+            TeleportEntity.entityTeleportWithProjectile(entity, this, mop, this.teleportDamage, true, true);
+        }
+
+        // Try to add the pearl straight back to the player's inventory
+        if (this.returnToPlayersInventory() == false)
+        {
+            this.dropAsItem();
+        }
+
+        this.setDead();
+    }
+
+    /**
+     * Tries to return the pearl back to the thrower's inventory.
+     * @return false if adding the item to the player's inventory failed and dropAsItem should be called. true if the entity can just be killed now.
+     */
+    public boolean returnToPlayersInventory()
+    {
+        if (this.canPickUp == false || this.worldObj.isRemote == true)
+        {
+            return true;
+        }
+
+        Entity thrower = this.getThrower();
+        int damage = (this.isElite == true ? 1 : 0);
+
+        // Tried to, but failed to add the pearl straight back to the thrower's inventory
+        if ((thrower instanceof EntityPlayer) == false ||
+           ((EntityPlayer)thrower).inventory.addItemStackToInventory(new ItemStack(EnderUtilitiesItems.enderPearlReusable, 1, damage)) == false)
+        {
+            return false;
+        }
+
+        return true;
     }
 
     public void dropAsItem()
