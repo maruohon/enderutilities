a83514e7da7c05232c7fbd3a9d275358523e826f
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2016-02-08 20:26:19 +0200

    Add back Builder's Wand ghost block rendering. *BROKEN*
    The faces seem to render in the wrong order, I might need to face sort all
    of them :/

--- src/main/java/fi/dy/masa/enderutilities/client/renderer/item/BuildersWandRenderer.java
+++ src/main/java/fi/dy/masa/enderutilities/client/renderer/item/BuildersWandRenderer.java
@@ -1,13 +1,16 @@
 package fi.dy.masa.enderutilities.client.renderer.item;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.lwjgl.opengl.GL11;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.RenderGlobal;
+import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.AxisAlignedBB;
@@ -22,6 +25,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import fi.dy.masa.enderutilities.item.ItemBuildersWand;
 import fi.dy.masa.enderutilities.item.ItemBuildersWand.Mode;
 import fi.dy.masa.enderutilities.setup.EnderUtilitiesItems;
+import fi.dy.masa.enderutilities.util.BlockInfo;
 import fi.dy.masa.enderutilities.util.BlockPosEU;
 import fi.dy.masa.enderutilities.util.BlockPosStateDist;
 import fi.dy.masa.enderutilities.util.nbt.NBTUtils;
@@ -169,32 +173,13 @@ public class BuildersWandRenderer
 
     public void renderGhostBlocks(EntityPlayer player, float partialTicks)
     {
-        // FIXME
-        /*RenderBlocks rb = new RenderBlocks();
-        //OpenGlHelper.glBlendFunc(774, 768, 1, 0);
-        //OpenGlHelper.glBlendFunc(770, 771, 1, 0);
-        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-        this.mc.getTextureManager().bindTexture(TextureMap.locationBlocksTexture);
-        GL11.glColor4f(1.0f, 1.0f, 1.0f, 0.4f);
-        GL11.glPushMatrix();
-        GL11.glEnable(GL11.GL_TEXTURE_2D);
-        GL11.glEnable(GL11.GL_CULL_FACE);
-        GL11.glEnable(GL11.GL_BLEND);
-
-        //GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-        GL11.glPolygonOffset(-3.0F, -3.0F);
-        GL11.glEnable(GL11.GL_POLYGON_OFFSET_FILL);
-        GL11.glAlphaFunc(GL11.GL_GREATER, 0.1F);
-        GL11.glDisable(GL11.GL_ALPHA_TEST);
-        GL11.glEnable(GL11.GL_DEPTH_TEST);
-        GL11.glDepthMask(false);
-        //OpenGlHelper.glBlendFunc(770, 1, 1, 0);
-        //OpenGlHelper.glBlendFunc(770, 771, 1, 0);
-        //GL11.glDepthMask(true);
-
         double dx = player.lastTickPosX + (player.posX - player.lastTickPosX) * partialTicks;
         double dy = player.lastTickPosY + (player.posY - player.lastTickPosY) * partialTicks;
         double dz = player.lastTickPosZ + (player.posZ - player.lastTickPosZ) * partialTicks;
+
+        GlStateManager.pushMatrix();
+        this.mc.getTextureManager().bindTexture(TextureMap.locationBlocksTexture);
+        GlStateManager.color(1f, 1f, 1f, 1f);
 
         if (partialTicks < this.partialTicksLast)
         {
@@ -215,25 +200,17 @@ public class BuildersWandRenderer
 
             if (blockInfo != null && blockInfo.block != null)
             {
-                GL11.glPushMatrix();
-                GL11.glTranslated(pos.posX - dx + 0.5d, pos.posY - dy + 0.5d, pos.posZ - dz + 0.5d);
+                IBlockState state = blockInfo.block.getStateFromMeta(blockInfo.blockMeta);
 
-                rb.renderBlockAsItem(blockInfo.block, blockInfo.meta, 1.0f);
+                GlStateManager.pushMatrix();
+                GlStateManager.translate(pos.posX - dx + 0.0d, pos.posY - dy + 0.0d, pos.posZ - dz + 1.0d);
 
-                GL11.glPopMatrix();
+                Minecraft.getMinecraft().getBlockRendererDispatcher().renderBlockBrightness(state, 1.0f);
+
+                GlStateManager.popMatrix();
             }
         }
 
-        GL11.glDisable(GL11.GL_TEXTURE_2D);
-        GL11.glDisable(GL11.GL_CULL_FACE);
-        GL11.glDisable(GL11.GL_BLEND);
-        GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
-        GL11.glDisable(GL11.GL_ALPHA_TEST);
-        GL11.glPolygonOffset(0.0F, 0.0F);
-        GL11.glDisable(GL11.GL_POLYGON_OFFSET_FILL);
-        GL11.glEnable(GL11.GL_ALPHA_TEST);
-        //GL11.glEnable(GL11.GL_DEPTH_TEST);
-        GL11.glDepthMask(true);
-        GL11.glPopMatrix();*/
+        GlStateManager.popMatrix();
     }
 }
