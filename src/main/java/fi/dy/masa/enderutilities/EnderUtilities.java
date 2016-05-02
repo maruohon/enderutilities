76c0e5e13fde0b2bf1debffb67c9c3073fe9100d
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2016-05-02 17:31:11 +0300

    Bump version and required Forge version
    Actual required Forge version is 1763+ for sure, but I'm not sure
    if there is something else in more recent builds that I'm using,
    so let's just require a recent-enough version...

--- src/main/java/fi/dy/masa/enderutilities/EnderUtilities.java
+++ src/main/java/fi/dy/masa/enderutilities/EnderUtilities.java
@@ -26,7 +26,7 @@ import fi.dy.masa.enderutilities.util.EnergyBridgeTracker;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION,
      guiFactory = "fi.dy.masa.enderutilities.setup.EnderUtilitiesGuiFactory",
-     dependencies = "required-after:Forge@[11.15.1.1722,);")
+     dependencies = "required-after:Forge@[11.15.1.1847,);")
 public class EnderUtilities
 {
     @Instance(Reference.MOD_ID)
