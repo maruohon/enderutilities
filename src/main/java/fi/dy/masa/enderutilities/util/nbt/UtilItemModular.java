c1d265a1d7a332a7bc177c257292291bb2175eb6
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2016-05-05 21:12:49 +0300

    Fix location binding coordinate offset
    Binding a location including the rotation used an incorrect method of
    getting the integer block position, which resulted in one-block offset
    when the coordinates are negative.

--- src/main/java/fi/dy/masa/enderutilities/util/nbt/UtilItemModular.java
+++ src/main/java/fi/dy/masa/enderutilities/util/nbt/UtilItemModular.java
@@ -757,16 +757,14 @@ public class UtilItemModular
      */
     public static void setTarget(ItemStack containerStack, EntityPlayer player, boolean storeRotation)
     {
-        int x = (int)player.posX;
-        int y = (int)player.posY;
-        int z = (int)player.posZ;
-        double hitX = player.posX - x;
-        double hitY = player.posY - y;
-        double hitZ = player.posZ - z;
+        BlockPos pos = player.getPosition();
+        double hitX = player.posX - pos.getX();
+        double hitY = player.posY - pos.getY();
+        double hitZ = player.posZ - pos.getZ();
         // Don't adjust the target position for uses that are targeting the block, not the in-world location
         boolean adjustPosHit = getSelectedModuleTier(containerStack, ModuleType.TYPE_LINKCRYSTAL) == ItemLinkCrystal.TYPE_LOCATION;
 
-        setTarget(containerStack, player, player.getPosition(), EnumFacing.UP, hitX, hitY, hitZ, adjustPosHit, storeRotation);
+        setTarget(containerStack, player, pos, EnumFacing.UP, hitX, hitY, hitZ, adjustPosHit, storeRotation);
     }
 
     /**
