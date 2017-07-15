00792a6c4f288b6459c200c634ec50dd2d855ed5
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2017-07-15 15:55:54 +0300

    Super minor GUI clean-up

--- src/main/java/fi/dy/masa/enderutilities/gui/client/base/GuiEnderUtilities.java
+++ src/main/java/fi/dy/masa/enderutilities/gui/client/base/GuiEnderUtilities.java
@@ -121,7 +121,7 @@ public class GuiEnderUtilities extends GuiContainer
             // Mouse is over the button
             if ((button instanceof GuiButtonHoverText) && button.mousePressed(this.mc, mouseX, mouseY))
             {
-                this.drawHoveringText(((GuiButtonHoverText)button).getHoverStrings(), mouseX, mouseY, this.fontRenderer);
+                this.drawHoveringText(((GuiButtonHoverText) button).getHoverStrings(), mouseX, mouseY, this.fontRenderer);
             }
         }
 
@@ -141,11 +141,8 @@ public class GuiEnderUtilities extends GuiContainer
             }
         }
 
-        int x = (this.width - this.xSize) / 2;
-        int y = (this.height - this.ySize) / 2;
-
         // Info text has been set, show it if the mouse is over the designated info area
-        if (this.infoArea != null && this.infoArea.isMouseOver(mouseX, mouseY, x, y))
+        if (this.infoArea != null && this.infoArea.isMouseOver(mouseX, mouseY, this.guiLeft, this.guiTop))
         {
             this.drawHoveringText(this.infoArea.getInfoLines(), mouseX, mouseY, this.fontRenderer);
         }
